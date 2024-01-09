7ce88e5316c54e67d9a4026c9bf537fd70e36355
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2024-01-09 15:26:15 +0300

    use canWrap trait

--- src/Tables/PopoverColumn.php
+++ src/Tables/PopoverColumn.php
@@ -4,6 +4,7 @@ namespace LaraZeus\Popover\Tables;
 
 use Filament\Support\Concerns;
 use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\Concerns\CanWrap;
 use Filament\Tables\Columns\Concerns\HasDescription;
 use LaraZeus\Popover\Concerns\HasPopover;
 
@@ -12,27 +13,14 @@ class PopoverColumn extends Column
     use Concerns\HasIcon;
     use HasPopover;
     use HasDescription;
+    use CanWrap;
 
     protected string $view = 'zeus-popover::popover';
-
-    protected bool | Closure $canWrap = false;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->disabledClick();
-    }
-
-    public function wrap(bool | Closure $condition = true): static
-    {
-        $this->canWrap = $condition;
-
-        return $this;
-    }
-
-    public function canWrap(): bool
-    {
-        return (bool) $this->evaluate($this->canWrap);
     }
 }
