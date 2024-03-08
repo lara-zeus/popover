52195aa75c220af5daf66eb3025cabada82e7d5a
Ash Monsh <wh7r.com@gmail.com>
2024-03-08 22:03:41 +0300

    Update PopoverEntry.php

--- src/Infolists/PopoverEntry.php
+++ src/Infolists/PopoverEntry.php
@@ -2,6 +2,7 @@
 
 namespace LaraZeus\Popover\Infolists;
 
+use Filament\Infolists\Components\Concerns\CanFormatState;
 use Filament\Infolists\Components\Concerns\HasIcon;
 use Filament\Infolists\Components\Entry;
 use LaraZeus\Popover\Concerns\HasPopover;
@@ -10,6 +11,7 @@ class PopoverEntry extends Entry
 {
     use HasIcon;
     use HasPopover;
+    use CanFormatState;
 
     protected string $view = 'zeus-popover::popover-entry';
 }
